b581b19b565dfb08b29ae2a7e05b3474e958c5a4
thangjik12 <kayami98@gmail.com>
2020-03-14 14:32:31 +0700

    first commit

---
+++ BACKEND/product_manager_api/routes/index.js
@@ -0,0 +1,64 @@
+var express = require('express');
+var router = express.Router();
+const { Pool, Client } = require('pg')
+const pool = new Pool({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'sanpham',
+  password: 'kayami98',
+  port: 5432,
+})
+
+
+router.get('/', function(req, res, next) {
+});
+
+
+// api get data from postgreSql 
+router.get('/getdata01', function(req, res, next) {
+  //  // Website you wish to allow to connect
+  //  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+
+  //  // Request methods you wish to allow
+  //  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  //  // Request headers you wish to allow
+  //  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  //  // Set to true if you need the website to include cookies in the requests sent
+  //  // to the API (e.g. in case you use sessions)
+  //  res.setHeader('Access-Control-Allow-Credentials', true);
+   
+  pool.query('SELECT * FROM product_info',(err, response) => {
+    if(err){
+      console.log(err);  
+    }
+    else {
+      res.send(response.rows);
+    }
+    //pool.end();
+  })
+  
+});
+
+router.get('/add', function(req, res, next) {
+  res.render('add',{})
+});
+
+router.post('/add', function(req, res, next) {
+  var product_name = req.body.product_name,
+  product_price = req.body.product_price,
+  image = req.body.image;
+  
+  pool.query("INSERT INTO product_info(product_name,product_price,image) values ($1,$2,$3)",
+  [product_name,product_price,image],(err,response)=>{
+    if(err) {
+      res.send(err)
+    }
+    else {
+      res.send('Đã insert dữ liệu thành công' + product_name + product_price + image);
+    }
+  })
+});
+
+module.exports = router;
